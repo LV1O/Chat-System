b715696e7ffec13ac82401a3417fcf5136110627
Lora Varagic <l.varagic.10@gmail.com>
2024-08-29 00:46:43 +1000

    Created Group 1 Page

---
+++ src/app/group-1-page/group-1-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-group-1-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './group-1-page.component.html',
+  styleUrl: './group-1-page.component.css'
+})
+export class Group1PageComponent {
+
+}
