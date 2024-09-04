634556aad7ef1cf9c417177a11c4b8b656592b95
Lora Varagic <l.varagic.10@gmail.com>
2024-09-04 21:14:59 +1000

    Added html elements and routing to Group1Page with updated styling, also added an input bar and submit button in preparation for phase 2

--- src/app/group1-page/group1-page.component.ts
+++ src/app/group1-page/group1-page.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-group1-page',
   templateUrl: './group1-page.component.html',
-  styleUrl: './group1-page.component.css'
+  styleUrls: ['./group1-page.component.css']
 })
 export class Group1PageComponent {
+  username: string | null = '';
+  message: string = ''; 
 
+  constructor(private router: Router) {}
+
+  back() {
+    this.router.navigate(['/groups']);
+  }
+
+  ngOnInit(): void {
+    this.username = localStorage.getItem('username');
+  }
+
+  submitMessage() {
+    if (this.message.trim()) {
+      console.log('Message submitted:', this.message);
+      this.message = ''; 
+    } else {
+      alert('Please enter a message.');
+    }
+  }
 }
